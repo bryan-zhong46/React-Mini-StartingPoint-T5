59bbe0ce596073601d9713fbcbb9e6f000cfbd26
Bryan Zhong <bryan.zhong46@stu.bmcc.cuny.edu>
2025-06-23 19:56:59 -0400

    Implemented Add Row button

--- src/app.jsx
+++ src/app.jsx
@@ -15,17 +15,38 @@ const initialGrid = [
   {rID: 2,
    cells: [{cID: 0}, {cID: 1}, {cID: 2}]
   },
-]
+];
 
 const App = () => {
   // define state variables
   const [grid, setGrid] = useState(initialGrid);
+  const rowCount = grid.length;
+  let colCount = 0;
+  if (grid.length !== 0) {
+    colCount = grid[0].cells.length;
+  }
+
+  function handleAddRow(colCount) {
+    // Create a new row with a unique rID and empty cells
+    const cellsArray = [];
+    for (let i = 0; i < colCount; i++) {
+      cellsArray.push({ cID: i });
+    }
+    const newRow = {
+      rID: rowCount,
+      cells: cellsArray
+    };
+    setGrid([...grid, newRow]);
+  }
+
 
   return (
     <div className="app">
       <h1 className="title">Grid Maker</h1>
 
       <Table grid={grid} />
+
+      <button className="add-row" onClick={() => handleAddRow(colCount)}>Add Row</button>
 
     </div>
   );
